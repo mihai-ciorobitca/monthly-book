d56971701d009f255caaccdd5cd398767c62990c
mihai-ciorobitca <mihai.ciorobitca@outlook.com>
2024-12-27 15:35:50 +0200

    upload

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ const app = express();
 const port = 3000;
 
 app.set('view engine', 'ejs');
-app.use(express.static('views'));
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
     (async () => {
