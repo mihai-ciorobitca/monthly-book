14f73df5ac2cb5549781ac9fd484031226dc8f23
mihai-ciorobitca <mihai.ciorobitca@outlook.com>
2024-12-27 15:29:33 +0200

    upload

---
+++ api/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const { createClient } = require('@supabase/supabase-js');
+
+require('dotenv').config();
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+
+const supabaseClient = createClient(supabaseUrl, supabaseKey);
+
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.use(express.static('views'));
+
+app.get('/', (req, res) => {
+    (async () => {
+        const { data, error } = await supabaseClient
+            .from('books')
+            .select('*');
+
+        if (error) {
+            console.error('Error fetching data:', error);
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+
+        books = data || [];
+
+        res.render('index', { books: data });
+    })();
+});
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
